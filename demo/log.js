9e9ce8a12a1f36dfb7f8668abe9cb14b2615c0db
helinjiang <onehlj@qq.com>
2021-05-01 16:57:37 +0800

    feat: 导出模块

--- demo/log.js
+++ demo/log.js
@@ -1,6 +1,6 @@
-const { log } = require('../');
+const { util } = require('../');
 
-const logger = log.createLogger('hh');
+const logger = util.log.createLogger('hh');
 
 logger.debug('我是 debug');
 logger.info('我是 info');
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,30 @@
+import * as base from './util/base';
+
 import * as log from './util/log';
+import * as npm from './util/npm';
+import * as port from './util/port';
+import * as runCmd from './util/run-cmd';
+import * as yaml from './util/yaml';
+import * as localCache from './tool/local-cache';
+import * as processHandler from './tool/process-handler';
+
+const util = {
+  base,
+  log,
+  npm,
+  port,
+  runCmd,
+  yaml,
+};
+
+export default {
+  util,
+  localCache,
+  processHandler,
+};
 
 export {
-  log
-}
+  util,
+  localCache,
+  processHandler,
+};
